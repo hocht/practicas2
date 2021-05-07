9c7a8a38ad23f93a6dfe5864b97054ee507ec823
hocht <rodrigoestillao@gmail.com>
2021-05-07 18:07:07 -0500

    Terminado capitulo de user inputs y while loops

--- Sandwiches ciclo while.py
+++ Sandwiches ciclo while.py
@@ -15,15 +15,31 @@ listaDePedidos = ['sandwich de jamon','sandwich de atun',
                   'sandwich de pavo y verduras','sandwich poblano',
                   'sandwich de aceitunas y jamon serrano','sandwich imperial',
                   'sandwich de atun','sandwich de pavo y verduras',
-                  'sandwich de pastrami','sandwich mediterraneo'
-                  'sandwich de pastrami','sandwich mediterraneo'
-                  'sandwich imperial','sandwich de asada']
+                  'sandwich de pastrami','sandwich mediterraneo',
+                  'sandwich de pastrami','sandwich mediterraneo',
+                  'sandwich imperial','sandwich de asada',
+                  'sandwich de tres quesos','sandwich de salmon',
+                  'sandwich de jamon','sandwich de tres quesos',
+                  'sandwich de atun','sandwich de pavo y verduras',
+                  'sandwich de tres quesos','sandwich de pastrami','sandwich imperial',
+                  'sandwich de frijoles','sandwich poblano','sandwich de jamon',
+                  'sandwich poblano','sandwich de atun','sandwich de tres quesos',
+                  'sandwich de jamon','sandwich de atun','sandwich de tres quesos']
 sandwichesListos = []
 
 while listaDePedidos:
     sandwich = listaDePedidos.pop()
-    print(f'Tu {sandwich.title()} se esta preparando \n' 
-           'Gracias por tu paciencia :)')
-    sandwichesListos.append(sandwich)
-
-print(f'estos fueron los sandwiches preparados{sandwichesListos}')
+    if sandwichesListos.count(sandwich) == 5:
+        print(f'Lo sentimos el {sandwich} se ha terminado :( '
+        '\nPuedes elegir uno diferente en el menu \n')
+        if listaDePedidos.count(sandwich) > 0:
+            listaDePedidos.remove(sandwich)
+    else:
+        print(f'Tu {sandwich.title()} se esta preparando \n' 
+        'Gracias por tu paciencia :) \n')
+        sandwichesListos.append(sandwich)
+    
+for sandwich in set(sandwichesListos):
+    print(f"el {sandwich} se preparo {sandwichesListos.count(sandwich)} veces")
+        
+            
