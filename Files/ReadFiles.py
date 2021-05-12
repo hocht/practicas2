cffbc67755d8f56f898a5d74e37806c2bc4936d5
hocht <rodrigoestillao@gmail.com>
2021-05-12 16:35:04 -0500

    practica de archivos

--- Files/ReadFiles.py
+++ Files/ReadFiles.py
@@ -2,4 +2,23 @@ filename = "files\python.txt"
 
 with open(filename) as file_object:
     contents = file_object.read()
-print(contents)
+    print(contents)
+    print("#################################")
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
+    print("#################################")
+with open(filename) as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        print(line.rstrip())
+print("#################################")
+with open(filename) as file_object:
+    lines = file_object.readlines()
+    for line in lines:
+        line = line.replace("in Python you can","in your couch you cannot")
+        print(line.rstrip())
+
+
+
